f674a77a9279a12394d9638832bbae3247949793
prakhar <prakharghatia@gmail.com>
2021-07-02 14:46:50 +0530

    commit mz

--- NewApp/app/src/main/java/com/example/newapp/MainActivity.kt
+++ NewApp/app/src/main/java/com/example/newapp/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        print("This is main branch");
+        print("This is mz branch");
         setContentView(R.layout.activity_main)
     }
 }
